47a93df745a34137200f7210cedfef03f5307eaa
abolfazl <maleki.direct@gmail.com>
2026-07-24 10:29:52 +0330

    Add GetZones, SetHero, IsHeroAvailable and GetHero to SPace.cpp

---
+++ src/Space.cpp
@@ -0,0 +1,36 @@
+#include "Space.h"
+
+bool Space::IsSecretPassage() const
+{
+    return HaveSecretPassage; 
+}
+
+void Space::SetSecretPassage(bool secret)
+{
+    HaveSecretPassage = secret;
+}
+
+int Space::GetId() const
+{
+    return Id;
+}
+
+const std::vector<ZoneTypes>& Space::GetZones() const
+{
+    return zones;
+}
+
+void Space::SetHero(Hero* Hero)
+{
+    hero = Hero;
+}
+
+Hero* Space::GetHero() const
+{
+    return hero; 
+}
+
+bool Space::IsHeroAvailable() const
+{
+    return !(hero == nullptr);
+}

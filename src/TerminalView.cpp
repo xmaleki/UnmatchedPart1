47183592fb5e784494235c1eb1b29ef2f55d0607
abolfazl <maleki.direct@gmail.com>
2026-07-26 11:14:33 +0330

    Add displayAsciiMap to TerminalView.cpp

--- src/TerminalView.cpp
+++ src/TerminalView.cpp
@@ -54,3 +54,84 @@ void TerminalView::display() const
     cout << "Space with * is Secret Passage\n";
     cout << "=========================================\n" << endl;
 }
+
+void TerminalView::displayAsciiMap() const
+{
+    vector<string> canvas(MAP_HEIGHT, string(MAP_WIDTH, ' '));
+    canvas[0] = "+" + string(MAP_WIDTH - 2, '-') + "+";
+    for (int i = 1; i < MAP_HEIGHT - 1; ++i)
+    {
+        canvas[i][0] = '|';
+        canvas[i][MAP_WIDTH - 1] = '|';
+    }
+    canvas[MAP_HEIGHT - 1] = "+" + string(MAP_WIDTH - 2, '-') + "+";
+
+    set<pair<int, int>> drawn_edges;
+    for (int u_id = 0; u_id < 32; ++u_id)
+    {
+        for (int v_id : map.GetAdjacents(u_id))
+        {
+            int u = min(u_id, v_id);
+            int v = max(u_id, v_id);
+            if (drawn_edges.insert({u, v}).second)
+            {
+                drawLine(canvas, nodeCoordinates.at(u), nodeCoordinates.at(v));
+            }
+        }
+    }
+
+    std::map<pair<int, int>, int> nodeStart;
+
+    for (const auto& p : nodeCoordinates)
+    {
+        nodeStart[{p.second.x - 2, p.second.y}] = p.first;
+    }
+
+    for (int y = 0; y < MAP_HEIGHT; ++y)
+    {
+        for (int x = 0; x < MAP_WIDTH; )
+        {
+            auto it = nodeStart.find({x, y});
+            if (it == nodeStart.end())
+            {
+                cout << canvas[y][x];
+                x++;
+                continue;
+            }
+
+            int id = it->second;
+            const Space& space = map.GetSpace(id);
+            const auto& zones = space.GetZones();
+
+            auto getColor = [&](ZoneTypes z)
+            {
+                return zoneColorMap.count(z) ? zoneColorMap.at(z) : Color::RESET;
+            };
+
+            string num = (id < 10) ? "0" + to_string(id) : to_string(id);
+            string heroSymbol = board.IsOccupied(id) ? GetHeroSymbol(board.GetOccupiedHeroId(id)) : "";
+
+            string c0 = zones.size() > 0 ? getColor(zones[0]) : Color::RESET;
+            string c1 = zones.size() > 1 ? getColor(zones[1]) : c0;
+            string c2 = zones.size() > 2 ? getColor(zones[2]) : c1;
+
+            if (zones.empty())
+            {
+                cout << "[" << num << heroSymbol << "]";
+            }
+            else
+            {
+                cout<< c0 << "[" << c1 << num[0] << c2 << num[1] << (heroSymbol.empty() ? "" : Color::RESET + heroSymbol + c0) << "]" << Color::RESET;
+            }
+
+            if (space.IsSecretPassage())
+            {
+                cout << "*";
+            }
+
+            int printedLen = 4 + heroSymbol.length() + (space.IsSecretPassage() ? 1 : 0);
+            x += printedLen;
+        }
+        cout << '\n';
+    }
+}

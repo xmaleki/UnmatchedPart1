e5e35457d0d21e972f5cf765897081358ea3e7e2
abolfazl <maleki.direct@gmail.com>
2026-07-27 07:19:52 +0330

    Add SetSideKicks, GetHero, GetHeroId, GetAge and GetName to Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,3 +12,31 @@ void Player::SetHero(std::unique_ptr<Hero> hero, int heroid)
     HERO = std::move(hero);
     HeroId = heroid;
 }
+
+void Player::SetSideKicks(std::unique_ptr<Hero> sidekick, int sidekickid)
+{
+    SIDEKICKS.push_back(std::move(sidekick));
+    SIDEKICKID.push_back(sidekickid);
+}
+
+Hero* Player::GetHero() const
+{
+    return HERO.get();
+}
+
+int Player::GetHeroId() const
+{
+    return HeroId;
+}
+
+
+std::string Player::GetAge() const
+{
+    std::string Age = std::to_string(Year) + "/" + std::to_string(Month) + "/" + std::to_string(Day);
+    return Age;
+}
+
+std::string Player::GetName() const
+{
+    return Name;
+}

b78d61d0a93a6f074c6b4f106b17c619eef25130
abolfazl <maleki.direct@gmail.com>
2026-07-28 08:19:35 +0330

    Add StartGame to GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -16,3 +16,76 @@ GameManager::GameManager()
 {
     GameMap = std::make_unique<Map>();
 }
+
+void GameManager::StartGame()
+{
+    cout<<"===================== Unmatched =====================\n";
+
+    int Year1, Month1, Day1;
+    int Year2, Month2, Day2;
+    string NameOfPlayer1, NameOfPlayer2;
+
+
+    cout<<"Enter name of player number 1 : ";
+    cin>>NameOfPlayer1;
+
+    cout<<"Enter age of player number 1"<<endl;
+    cout<<"Year: ";
+    cin>>Year1;
+    cout<<"Month: ";
+    cin>>Month1;
+    cout<<"Day: ";
+    cin>>Day1;
+
+    cout<<"Enter name of player number 2 : ";
+    cin>>NameOfPlayer2;
+
+    cout<<"\nEnter age of player number 2:\n";
+    cout<<"Year: ";
+    cin>>Year2;
+    cout<<"Month: ";
+    cin>>Month2;
+    cout<<"Day: ";
+    cin>>Day2;
+
+
+    // mishe operator overload kard. badan age shod in karo mikonam.
+    if(Year1 > Year2)
+    {
+        player1 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+        player2 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+    }
+    else if(Year1 < Year2)
+    {
+        player1 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+        player2 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+    }
+    else
+    {
+        if(Month1 > Month2)
+        {
+            player1 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+            player2 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+        }
+        else if(Month1 < Month2)
+        {
+            player1 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+            player2 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+        }
+        else
+        {
+            if(Day1 >= Day2)
+            {
+                player1 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+                player2 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+            }
+            else if(Day2 > Day1)
+            {
+                player1 = make_unique<Player>(NameOfPlayer2 ,Year2, Month2, Day2);
+                player2 = make_unique<Player>(NameOfPlayer1 ,Year1, Month1, Day1);
+            }
+        }
+    }
+    
+}
+

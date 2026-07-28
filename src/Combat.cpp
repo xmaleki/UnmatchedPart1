1e55d8d030ed4a51c58262b48bbbd8b008195f1b
abolfazl <maleki.direct@gmail.com>
2026-07-28 08:13:58 +0330

    Add StartCombat to Combat.cpp

--- src/Combat.cpp
+++ src/Combat.cpp
@@ -4,3 +4,159 @@
 
 Combat::Combat(Board& b, Map& m, Movement&mv):board(b), map(m), movement(mv)
 {}
+
+void Combat::StartCombat(Player* AttackerPlayer, Player* DefenderPlayer,
+                        Hero* Attacker, Hero* Defender, Card* AttackCard)
+{
+    CombatContext combatcontext
+    {
+        AttackerPlayer, DefenderPlayer, Attacker, Defender, AttackCard->GetValue(), 0, board, map, movement, AttackCard
+    };
+
+    cout<<"==========================================\n";
+    cout<<"Combat\n";
+    cout<<"==========================================\n";
+
+    cout<<"-------------------- Before Combat --------------------\n";
+    cout<<"Attacker HP: "<<Attacker->GetHP()<<endl;
+    cout<<"Defender HP: "<<Defender->GetHP()<<endl;
+    cout<<"-------------------- Before Combat --------------------\n";
+    
+    // system combat bayad ghabl az ejraye code ha, feint ro baraye cancel kardan check kone.
+
+    cout<<Attacker->GetName()<<" attacks "<<Defender->GetName()<<" !\n";
+    
+    AttackerPlayer->DiscardCardFromHand(AttackCard);
+
+    Card* DefenderCard = nullptr;
+
+    auto& DeffenderHand = DefenderPlayer->GetHand();
+    vector<int> PlayableDefenderCards;
+    
+    int counter = 1;
+
+    for(int i = 0; i < DeffenderHand.size(); i++)
+    {
+        Card* c = DeffenderHand[i].get();
+        if(c->GetType() == CardType::Defence || c->GetType() == CardType::Versatile)
+        {
+            PlayableDefenderCards.push_back(i);
+            cout<<counter<<") "<<c->GetName()<<" (Value:"<<c->GetValue()<<") "<<"["<<c->ToStringType(c->GetType())<<"]"<<"\n";
+            counter++;
+        }
+    }
+    
+
+    if(!PlayableDefenderCards.empty())
+    {
+        cout<<counter<<") Play no defense card\n";
+
+        while (true)
+        {
+            cout<<"Please enter the card for defense: ";
+            int choice;
+            cin>>choice;
+
+            if(choice == counter)
+            {
+                //cout<<"No choice\n";
+                break;
+            }
+            else if(choice > 0 && choice <= PlayableDefenderCards.size())
+            {
+                DefenderCard = DeffenderHand[PlayableDefenderCards[choice - 1]].get();
+                DefenderPlayer->DiscardCardFromHand(DefenderCard);
+                combatcontext.DefenseCard = DefenderCard;
+                break;
+            }
+            else
+            {
+                cout<<"Invalid choice. try again!\n";
+            }
+        }
+    }
+    else
+    {
+        cout<<"Defender has no defense cards.\n";
+        DefenderCard = nullptr;
+    }
+    
+
+    cout<<"Attacker played : "<<AttackCard->GetName()<<"\n";
+    if(DefenderCard)
+    {
+        cout<<"Defender played: "<<DefenderCard->GetName()<<"\n";
+    }
+    else
+    {
+        cout<<"Defender played no card.\n";
+    }
+
+    bool DefenderPlayedFeint = (DefenderCard && DefenderCard->GetName() == "Feint");
+    bool AttackerPlayedFeint = (AttackCard->GetName() == "Feint");
+
+    bool SherlockDSAP = (DefenderCard) && (DefenderCard->GetOwner() == CardOwner::SherlockHolmes || DefenderCard->GetOwner() == CardOwner::DrWatson); 
+    bool SherlockASAP = (AttackCard->GetOwner() == CardOwner::SherlockHolmes || AttackCard->GetOwner() == CardOwner::DrWatson);
+
+    // immediately
+    if(DefenderCard)
+        DefenderCard->ApplyImmediately(combatcontext);
+    
+    if(!DefenderPlayedFeint || SherlockASAP)
+        AttackCard->ApplyImmediately(combatcontext);
+
+    // during combat
+    if(DefenderCard)
+    {
+        if(!AttackerPlayedFeint || SherlockDSAP)
+            DefenderCard->ApplyDuringCombat(combatcontext);
+    }
+
+    if(!DefenderPlayedFeint || SherlockASAP)
+        AttackCard->ApplyDuringCombat(combatcontext);
+
+    if(combatcontext.DefenseValue == 0 && DefenderCard)
+    {
+        combatcontext.DefenseValue = DefenderCard->GetValue();
+    }
+
+    int Damage = combatcontext.AttackValue - combatcontext.DefenseValue;
+    
+    if(Damage < 0)
+    {
+        Damage = 0;
+        combatcontext.AttackerWin = false;
+    }
+    cout<<"Damage = "<<Damage<<"\n";
+
+    if(Damage > 0)
+    {
+        Defender->TakeDamage(Damage);
+        cout<<Defender->GetName()<<" takes "<<Damage<<" damage!\n";
+        combatcontext.AttackerWin = true;
+    }
+    else
+    {
+        cout<<"No Damage dealt.\n";
+    }
+
+    // after combat effect
+    if(DefenderCard)
+    {
+        if(!AttackerPlayedFeint || SherlockDSAP)
+            DefenderCard->ApplyAfterCombat(combatcontext);
+    }
+
+    if(!DefenderPlayedFeint || SherlockASAP)
+        AttackCard->ApplyAfterCombat(combatcontext);
+
+    cout<<"-------------------- After Combat --------------------\n";
+    cout<<"Attacker HP: "<<Attacker->GetHP()<<endl;
+    cout<<"Defender HP: "<<Defender->GetHP()<<endl;
+    cout<<"-------------------- After Combat --------------------\n";
+
+    cout<<"==========================================\n";
+    cout<<"Combat Ended\n";
+    cout<<"==========================================\n";
+
+}
